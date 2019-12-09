92e98fb55b8f523fa9f10c9ab7b77e83f7496286
Alexandre Andrieux <alexandre.andrieux@cgi.com>
2019-12-09 18:26:58 +0100

    Add exercice 1 correction

--- exercice1/helloWorld.php
+++ exercice1/helloWorld.php
@@ -4,6 +4,7 @@
 /* Display here "Hello World";       */
 /*************************************/
 
+echo "Hello World";
 
 // You can use one of this function :
 // 'echo' https://www.php.net/manual/fr/function.echo.php
@@ -13,6 +14,8 @@
 /* Display here value sent by user;  */
 /*************************************/
 
+$line = readline("What do you want to write ?");
+print_r($line);
 
 // To check what you type, use 'readline'
 // https://www.php.net/manual/fr/function.readline.php
