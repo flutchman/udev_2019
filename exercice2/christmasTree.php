9ba5cad2de00d04d224eba856ad222da322909e4
Alexandre Andrieux <alexandre.andrieux@cgi.com>
2019-12-09 10:38:44 +0100

    Add exercice 2

---
+++ exercice2/christmasTree.php
@@ -0,0 +1,20 @@
+<?php
+
+/*************************************/
+/* Set here its size;                */
+/*************************************/
+
+
+/*************************************/
+/* Display here the Tree !           */
+/*************************************/
+
+// Use 'for' https://www.php.net/manual/fr/control-structures.for.php
+// Use 'str_repeat' https://www.php.net/manual/fr/function.str-repeat.php
+// First use ' ' and THEN "*"
+
+
+// Don't forget the trunk !
+
+
+echo "\n";
