3b6e2eb300aa72f5662685ce6498b6150eb4f8be
Alexandre Andrieux <alexandre.andrieux@cgi.com>
2019-12-09 18:27:19 +0100

    Add exercice 2 correction

--- exercice2/christmasTree.php
+++ exercice2/christmasTree.php
@@ -4,6 +4,7 @@
 /* Set here its size;                */
 /*************************************/
 
+$line = readline("How much this tree has to be ? ");
 
 /*************************************/
 /* Display here the Tree !           */
@@ -13,8 +14,22 @@
 // Use 'str_repeat' https://www.php.net/manual/fr/function.str-repeat.php
 // First use ' ' and THEN "*"
 
+for ($i = 1; $i < $line; $i++) {
+    echo str_repeat(" ", $line - $i);
+    for ($j = 0; $j < $i; $j++) {
+        echo "*";
+    }
+    echo str_repeat("*", $i - 1);
+    echo "\n";
+}
 
 // Don't forget the trunk !
 
+$trunk = $line / 3;
+for ($i = 0; $i < 3; $i++) {
+    echo str_repeat(" ", ($line - $trunk));
+    echo str_repeat("*", $line - $trunk);
+    echo "\n";
+}
 
 echo "\n";
