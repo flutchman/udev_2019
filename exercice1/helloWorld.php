ed614a9e24541aee1446edb6e24630dd19f89d89
Alexandre Andrieux <alexandre.andrieux@cgi.com>
2019-12-09 10:38:01 +0100

    Add exercice 1

---
+++ exercice1/helloWorld.php
@@ -0,0 +1,20 @@
+<?php
+
+/*************************************/
+/* Display here "Hello World";       */
+/*************************************/
+
+
+// You can use one of this function :
+// 'echo' https://www.php.net/manual/fr/function.echo.php
+// 'print' https://www.php.net/manual/fr/function.print.php
+
+/*************************************/
+/* Display here value sent by user;  */
+/*************************************/
+
+
+// To check what you type, use 'readline'
+// https://www.php.net/manual/fr/function.readline.php
+
+echo "\n";
